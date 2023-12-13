82a80ac9a00ce7cc9759f42c0f1839cc27225ec1
parihar-abhishek <pariharabhishek227@gmail.com>
2023-12-13 23:42:06 +0530

    new code

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -10,22 +10,31 @@ const Admin = () => {
   const [data, setData] = useState(null)
 
   const readExcel = async (e) => {
-    const file = e.target.files[0]
-    const data = await file.arrayBuffer(file)
-    const excelfile = xlsx.read(data)
-    const excelsheet = excelfile.Sheets[excelfile.SheetNames[0]]
-    const exceljson = xlsx.utils.sheet_to_json(excelsheet)
+    const file = e.target.files[0];
+    const data = await file.arrayBuffer(file);
+    const excelfile = xlsx.read(data);
+    const excelsheet = excelfile.Sheets[excelfile.SheetNames[0]];
+    const exceljson = xlsx.utils.sheet_to_json(excelsheet);
 
-    setData(exceljson)
-  }
-  console.log(data)
+    
+
+    setData(exceljson);
+
+};
+ console.log(data);
+
+ 
+
+
+
 
   return (
     <div>
       admin pannel
       <br />
       <input type="file" onChange={(e) => readExcel(e)} />
-      <button onClick={() => addData(data)}> Upload </button>
+
+      <button onClick={()=>addData(data)}>  import  </button>
     </div>
   )
 }
